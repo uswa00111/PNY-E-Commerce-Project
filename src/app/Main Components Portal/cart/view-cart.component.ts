0d71a7ea765f265236d7a5cbdb5b2c990269a897
uswa.r <uswa.r@outlook.com>
2022-07-17 14:49:58 +0500

    error trial

--- src/app/Main Components Portal/cart/view-cart.component.ts
+++ src/app/Main Components Portal/cart/view-cart.component.ts
@@ -60,58 +60,26 @@ export class ViewCartComponent implements OnInit {
     this.ProductQuantity = this.FilteredArray[0].qty;
   }
 
-  // Increment(Data) {
-  //   this.cartItemsTotal = 0;
-  //   let _DataIdentity = Data.CartId;
-  //   let _DataQuantity = Data.CartItemQuantity;
-  //   this.cartItems.map(CartData => {
-  //     if (CartData.productId === _DataIdentity) {
-  //       if (_DataQuantity < CartData.TotalQuantity) {
-  //         CartData.qty++;
-  //         this._AddToCartService.SetCartToLocalStorage(this.cartItems);
-  //         this.cartTotal = 0;
-  //         this.cartItems.forEach(item => {
-  //           this.cartTotal += (item.price * item.qty);
-  //           this._AddToCartService.SetCartTotal(this.cartTotal);
-  //         })
-  //       } else {
-  //         return null;
-  //       }
-  //     }
-  //   })
-  //   this.cartItems.map(_Data => {
-  //     this.cartItemsTotal = this.cartItemsTotal + _Data.qty;
-  //     this._TotalItems = this.cartItemsTotal;
-  //   })
-  //   this._AddToCartService.SetNumberOfTotalCartItems(this.cartItemsTotal);
-
-  // }
 
   AddQuantity() {
     this.ShowBox = true;
     let CartObjectPlus = this._NonVolatileService.GetProdcutToLocalStorage();
-    //Agar Quantity Increase Kar Jae New Quanityt To Khatam Kar do Yeh Fucntion
     if (this.SelectedQuantity >= this.ProductQuantity) {
       this._ToastrService.error('Quantiyy cannot Exceed that Original Quanityt');
       return
     }
-    
-    //First Time Jaab LocalStorage main Khuch nahi Para Hua
 
-    if(Object.entries(CartObjectPlus).length === 0){ //iss line ka mtalab k jo data(LS) check karo k wo empty hai ya nahi
-      this.CartArray.push(this.FilteredArray[0]);//CartArray(Variable) Push Filter Array ko Push (Filter Array App ka wo Data Object hai jo App apnay Json Data say filter kar raya hon on base of Id)
-      this._NonVolatileService.AddProdcutToLocalStorage(this.CartArray);// Local Storage main Data Add (Create)
-      this.Data = undefined;//Yeh hauamain Bataye ga k hum same Object(Data) pay kharay hai k nahi
-      this.LocalStorageCartArray = this._NonVolatileService.GetProdcutToLocalStorage();// Yeh main Data Local Storage say Dubara get kar raha hoon (Get/Read)
+    if(Object.entries(CartObjectPlus).length === 0){ 
+      this.CartArray.push(this.FilteredArray[0]);
+      this._NonVolatileService.AddProdcutToLocalStorage(this.CartArray);
+      this.Data = undefined;
+      this.LocalStorageCartArray = this._NonVolatileService.GetProdcutToLocalStorage();
       return
     }
 
-    //Abb LoaclStorage main Data Para Hua hai Lakin Hum nay dekhana hai k Quantity Increase hoo lakin jaab new 
-    // Data Peechay say Aye Taab wo Push karay Sara
-
-    if(Object.entries(CartObjectPlus).length !== 0 && this.Data !== undefined){//Jab Home/Product say New Data Object Aye
-      this.NewCartArray.push(this.FilteredArray[0]); //Jo New Object Data aa raha hai uss ko NewArray(variable/property) Main Push kar do
-      CartObjectPlus.forEach((element:any) => {//Local sotrage array of objects pay loop aur step by step un objects ko NewArray Push karo taka NewObjec jo k product say aa raha hai wo local storage k objects k sath mill jae
+    if(Object.entries(CartObjectPlus).length !== 0 && this.Data !== undefined){
+      this.NewCartArray.push(this.FilteredArray[0]); 
+      CartObjectPlus.forEach((element:any) => {
         this.NewCartArray.push(element);
       });
       this._NonVolatileService.AddProdcutToLocalStorage(this.NewCartArray);
@@ -120,10 +88,10 @@ export class ViewCartComponent implements OnInit {
       return
     }
 
-      CartObjectPlus.map((element:any) => { //Yahan main Loop Kara raha hoon Local Storage say jo Array of Objetcs aa rahi hai
-        if(this.FilteredArray[0]._id === element._id){ // Mujhay pata nahi k Localstorage say jo Array aa rahi uss main kitnay obketcs hain to Main filter k Meri localstroage ke array of objetcs ke id === FilterArray[0]
-          element.NewQuantity++;//Increment  (Update)
-          this.SelectedQuantity++; // Increament (Update)
+      CartObjectPlus.map((element:any) => { 
+        if(this.FilteredArray[0]._id === element._id){ 
+          element.NewQuantity++;
+          this.SelectedQuantity++; 
         }
        });
        this._NonVolatileService.AddProdcutToLocalStorage(CartObjectPlus);
@@ -147,8 +115,4 @@ export class ViewCartComponent implements OnInit {
      this._NonVolatileService.AddProdcutToLocalStorage(CartObjectPlus);
      this._NonVolatileService.SetUserMiscellaneousInformation(this.CartQuantity);
   }
-
-
-
-  
 }
